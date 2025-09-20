7321cded082b7cf011890a8468e0d67ac0c60057
rj-adity <75466680+rj-adity@users.noreply.github.com>
2025-09-20 16:49:19 +0530

    #bug fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
